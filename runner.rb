fc65c4bac343a6f043e5755a7c807a39bf540c30
[01;31m[K"name"[m[K: "Catherine Khuu", &   [01;31m[K"name"[m[K: "Nan Min", &   [01;31m[K"name"[m[K: "Wellington Vicioso <pair+catkhuu.nanutza.wvicioso@devbootcamp.com>
2016-09-18 23:24:06 -0400

    Develop a class-based recursive Sudoku Solution that solves the first 5 boards

---
+++ runner.rb
@@ -0,0 +1,33 @@
+require_relative 'sudoku'
+
+# The sudoku puzzles that your program will solve can be found
+# in the sudoku_puzzles.txt file.
+#
+# Currently, Line 18 defines the variable board_string to equal
+# the first puzzle (i.e., the first line in the .txt file).
+# After your program can solve this first puzzle, edit
+# the code below, so that the program tries to solve
+# all of the puzzles.
+#
+# Remember, the file has newline characters at the end of each line,
+# so we call String#chomp to remove them.
+
+File.readlines('sudoku_puzzles.txt').each_with_index do |line, index|
+board_string = line.chomp
+
+
+board = Sudoku.new(board_string)
+
+
+  board.solve
+
+  if board.solved?
+
+    puts "\nBoard number - #{index + 1} was solved!\n\n"
+    print board.pretty_board
+  else
+    puts "\nBoard number - #{index + 1} wasn't solved :( \n\n"
+  end
+
+end
+
---
+++ sudoku.rb
@@ -0,0 +1,119 @@
+require 'pry'
+
+class Sudoku
+  attr_accessor :board, :board_array
+  attr_reader :possibles
+
+      STARTING_POINTS = {
+      [0,0] => {0=>0},
+      [0,1] => {1=>3},
+      [0,2] => {2=>6},
+      [1,0] => {3=>27},
+      [1,1] => {4=>30},
+      [1,2] => {5=>33},
+      [2,0] => {6=>54},
+      [2,1] => {7=>57},
+      [2,2] => {8=>60}
+    }
+ 
+
+  def initialize(board_string)
+    @board = String.new(board_string)
+    @possibles = '123456789'
+  end
+
+  def solve
+    unless solved?
+      board.split('').each_with_index do |element, idx|
+
+        if element == '-'
+          possible_nums = current_possibles(idx)        
+          update_board(idx, possible_nums) if single_solution?(possible_nums)
+        end
+      end
+          solve if board.split('').any? { |element| element.include?("-")} 
+    end
+  end
+
+  def solved?
+    board.split('').reduce(0){|total, element| total += element.to_i} == 405 && 
+    !board.include?("-")
+  end
+
+  def board_to_array(board)
+    board_copy = String.new(board)
+    board_copy.chars.each_slice(9).to_a
+  end
+
+  def get_cols(idx)
+    col_num = idx % 9
+    board_arr = board_to_array(board)
+    board_arr.transpose[col_num].join
+  end 
+ 
+  def get_rows(idx)
+    row_num = idx / 9 
+    board_arr = board_to_array(board)
+    board_arr[row_num].join
+  end
+
+
+  def find_super_box_index(value_index)
+    box_row = value_index / 27
+    box_col = value_index / 3 % 3
+    STARTING_POINTS[[box_row,box_col]].values.first
+  end 
+
+
+  def get_box(box_start)
+    x = box_start
+    super_box = ''
+    3.times do
+        super_box += board[x..x+2]
+        x += 9
+    end
+    super_box
+  end 
+
+
+
+  def current_possibles(idx)
+    box = get_box(find_super_box_index(idx))
+    row = get_rows(idx)
+    col = get_cols(idx)
+    delete_this = box + row + col
+
+    updated_possibles = possibles.delete delete_this.delete("-")
+    updated_possibles
+  end
+
+  def single_solution?(possible_nums)
+    possible_nums.length == 1
+  end
+
+  def more_than_one_solution(possible_nums)
+    possible_nums.length > 1
+  end 
+
+  def guess_number(idx, possible_nums)
+    @board[idx] = possible_nums.split("").sample.to_s
+  end 
+
+  def update_board(idx, possible_nums)
+    @board[idx] = possible_nums[0].to_s
+  end
+
+def pretty_board
+  printable_board = board_to_array(board)
+  printable_board.map do |row|
+    if row.kind_of?(Array)
+      row.insert(3, "|")
+      row.insert(7, "|")
+      row.flatten.join(" ")
+    end
+  end
+  printable_board.flatten.join(" ")
+end
+
+end
+
